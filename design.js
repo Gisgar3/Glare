8b534e1ca607707f9e2df5f1f9382cc92ed6e6cb
Gavin Isgar <gisgar3@gmail.com>
2019-04-07 03:59:59 -0400

    Tons of updated design and logic
    Updated design and logic attributes.

--- design.js
+++ design.js
@@ -3,16 +3,25 @@ const userconfig = require("./config/user.json");
 var dateandtime = new Date();
 var fulltime = dateandtime.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
 
-var date_element = document.getElementById("header");
-date_element.innerHTML = null;
-date_element.innerHTML = `Good morning, ${userconfig.username}`;
+var dateheader_element = document.getElementById("header");
+var bottombar_element = document.getElementById("bottombar");
+//bottombar_element.style.width = screen.width.toString() - "200";
+dateheader_element.innerHTML = null;
+console.log(dateandtime.getHours());
+if (dateandtime.getHours() >= 0 && dateandtime.getHours() < 12) {
+    dateheader_element.innerHTML = `<h1>Good morning, ${userconfig.username}</h1>`;
+}
+if (dateandtime.getHours() > 11 && dateandtime.getHours() < 24) {
+    dateheader_element.innerHTML = `<h1>Good afternoon, ${userconfig.username}</h1>`;
+}
 
 function colorOptionChange() {
-    if (document.getElementById("selector").getElementsByTagName("option")[0].selected == true) {
-        document.getElementById("body").setAttribute("style", "background-color:white; animation-name: colorfadetolight; animation-duration: 2s;");
+    if (document.getElementById("colorswitch").getElementsByTagName("input")[0].checked == false) {
+        document.getElementById("body").setAttribute("style", "background-color:white; animation-name: colorfadetolight; animation-duration: 1s;");
+        document.getElementById("header").setAttribute("style", "color:black; animation-name: textfadeforlight; animation-duration: 2s;");
     }
     else {
-        document.getElementById("body").setAttribute("style", "background-color:gray; animation-name: colorfadetodark; animation-duration: 2s;");
-        
+        document.getElementById("body").setAttribute("style", "background-color:rgb(21, 23, 25); animation-name: colorfadetodark; animation-duration: 1s;");
+        document.getElementById("header").setAttribute("style", "color:white; animation-name: textfadefordark; animation-duration: 2s;");
     }
 }
--- template.js
+++ template.js
@@ -3,14 +3,17 @@ var page = `
     <html>
         <head>
             <meta charset="UTF-8">
+            <meta name="viewport" content="width=1920, maximum-scale=1">
             <title>Glare</title>
         </head>
         <body id="body">
-            <center><h1 id="header">Good morning!</h1></center>
-            <select id="selector" onchange="colorOptionChange()">
-                <option value="light">Light</option>
-                <option value="dark">Dark</option>
-            </select>
+            <center><h1 id="header">N/A</h1></center>
+            <div class="bottombar" id="bottombar">
+                <label class="switch" id="colorswitch" onchange="colorOptionChange()">
+                    <input type="checkbox">
+                    <span class="slider round"></span>
+                </label>
+            </div>
             <script src="./design.js"></script>
         </body>
         <style>
@@ -20,25 +23,101 @@ var page = `
             }
             h1 {
                 font-family: "TitilliumWebFont";
-                font-size: 80px;
-                padding-top:340px;
+                font-size: 75px;
+                padding-top:140px;
                 animation-name: fadein;
                 animation-duration: 2s;
             }
             h2 {
                 font-family: "TitilliumWebFont";
             }
+            .bottombar {
+                opacity: 0.2; 
+                border-top-left-radius: 20px; 
+                border-top-right-radius: 20px; 
+                height: 75px; 
+                width: 100%; 
+                bottom: 0px; 
+                left: 0px;
+                right: 0px;
+                position: fixed; 
+                border-style: solid; 
+                border-color: gray; 
+                background-color: rgb(81, 81, 81);
+            }
+
+            .switch {
+                position: relative;
+                display: inline-block;
+                width: 60px;
+                height: 34px;
+                margin-left: 35px;
+                margin-top: 5px;
+            }
+            .switch input {
+                opacity: 0;
+                width: 0;
+                height: 0;
+            }
+            .slider {
+                position: absolute;
+                cursor: pointer;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
+                background-color: #ccc;
+                -webkit-transition: .4s;
+                transition: .4s;
+            }
+            .slider:before {
+                position: absolute;
+                content: "";
+                height: 26px;
+                width: 26px;
+                left: 4px;
+                bottom: 4px;
+                background-color: white;
+                -webkit-transition: .4s;
+                transition: .4s;
+            }
+            input:checked + .slider {
+                background-color: #898989;
+            }
+            input:focus + .slider {
+                box-shadow: 0 0 1px #898989;
+            }
+            input:checked + .slider:before {
+                -webkit-transform: translateX(26px);
+                -ms-transform: translateX(26px);
+                transform: translateX(26px);
+            }
+            .slider.round {
+                border-radius: 34px;
+            }
+            .slider.round:before {
+                border-radius: 50%;
+            }
+
             @keyframes fadein {
                 from {opacity: 0.0;}
                 to {opacity: 1.0;}
             }
             @keyframes colorfadetodark {
                 from {background-color: white}
-                to {background-color: gray}
+                to {background-color: rgb(21, 23, 25)}
             }
             @keyframes colorfadetolight {
-                from {background-color: gray}
+                from {background-color: rgb(21, 23, 25)}
                 to {background-color: white}
+            }
+            @keyframe textfadefordark {
+                from {color: black}
+                to {color: white}
+            }
+            @keyframe textfadeforlight {
+                from {color: white}
+                to {color: black}
             }
     </html>
     `;
